2da449885483d86b9c119ffddb3a312f27c8db7f
muzia <3239167452@qq.com>
2016-04-07 11:56:28 +0800

    Revert "与主分支同步"

--- app/src/main/java/com/master/kit/TestActivity/SwitchButtonActivity.java
+++ app/src/main/java/com/master/kit/TestActivity/SwitchButtonActivity.java
@@ -1,6 +1,5 @@
 package com.master.kit.TestActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import com.master.kit.R;
 
-public class SwitchButtonActivity extends Activity {
+public class SwitchButtonActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
